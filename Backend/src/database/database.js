a10d2f8fb11154c0a95f745cf87030728b6961aa
Alexander <alexandermelian@outlook.com>
2023-12-16 18:26:59 -0300

    fix ssl

--- Backend/src/database/database.js
+++ Backend/src/database/database.js
@@ -5,6 +5,11 @@ export const sequelize = new Sequelize(
     process.env.DB_PASSWORD,{
         host: process.env.DB_HOST,
         dialect: "postgres",
-        ssl: true
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false, // Añadir esto para evitar problemas con certificados no confiables
+            },
+        },
     }
 ) 
