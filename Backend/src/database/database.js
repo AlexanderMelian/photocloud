13b16b4072cae1d4aaa2af0214cf6b6ae7647347
Alexander <alexandermelian@outlook.com>
2023-12-16 18:17:34 -0300

    add ssl

--- Backend/src/database/database.js
+++ Backend/src/database/database.js
@@ -4,6 +4,7 @@ export const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,{
         host: process.env.DB_HOST,
-        dialect: "postgres"
+        dialect: "postgres",
+        ssl: true
     }
 ) 
