15b7a12081333f42dee25d28cf80c3c0f6451bff
Alexander <alexandermelian@outlook.com>
2023-12-16 19:22:09 -0300

    env fix

--- Backend/src/database/database.js
+++ Backend/src/database/database.js
@@ -1,8 +1,8 @@
 import Sequelize from "sequelize"
 export const sequelize = new Sequelize(
-    String(process.env.DB_DATABASE),
-    String(process.env.DB_USER),
-    String(process.env.DB_PASSWORD),{
+    process.env.DB_DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,{
         host: process.env.DB_HOST,
         dialect: "postgres",
         dialectOptions: {
